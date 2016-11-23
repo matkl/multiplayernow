e3c850618df6e823c8004d2a65ccd809188f6557
Matthias Klein <matthias@klein.pw>
2016-11-23 08:58:55 +0100

    fix travis configuration

--- controllers/games.js
+++ controllers/games.js
@@ -2,11 +2,8 @@
  * Provide req.game.
  */
 exports.load = (req, res, next) => {
-  console.log('L1');
   req.db.collection('games').findOne({ _id: req.params.id })
   .then((doc) => {
-    console.log('L2');
-    console.log(req.game, req.path);
     req.game = doc;
     next();
   })
